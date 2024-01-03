38723835b4031af660f84795157c648ae281f50b
manish singh <38642903+manishsingh7@users.noreply.github.com>
2024-01-03 15:10:25 +0530

    Initial commit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const {
+  allUsers,
+  registerUser,
+  authUser
+} = require("../controllers/userControllers");
+const { protect } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.route("/").get(protect, allUsers);
+router.route("/").post(registerUser).get(allUsers);
+router.post("/login", authUser);
+
+module.exports = router;
